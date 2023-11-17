3e9d2e58c1f91be7af99557692f9970abe36d499
tanner666 <tjg5@njit.edu>
2023-11-16 20:03:43 -0500

    added tests and modified compose file

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
-# Playwright-Container
-# playwright commands:
-# npm init playwright@latest
-# npx playwright codegen http://localhost:3000/
-# npx playwright test
-# options: --debug, --project-firefox, etc
-# npx playwright show-report
-# Docker stuff:
-# docker build . --file Dockerfile.jammy -t playwright
-# docker run -it -d --name playwrightcontainer playwright /bin/bash
-# docker exec -it playwrightcontainer /bin/bash
-# npx playwright test
+# Running Playwright in a Container:
+## Playwright Commands:
+- npm init playwright@latest
+- npx playwright codegen http://localhost:3000/
+- npx playwright test
+  - options: --debug, --project-firefox, etc
+- npx playwright show-report
+## Docker Commands:
+- docker build . --file Dockerfile.jammy -t <image_name>
+  - builds image
+- docker run -it -d --name <container_name> <image_name> /bin/bash
+  - builds container
+- docker exec -it <container_name> /bin/bash
+  - use docker container terminal
+- npx playwright test
+  - run test inside of container  (after previous command)
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,29 +1,74 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
+  await page.goto('https://is373-t3-stack.vercel.app/');
   await page.getByRole('heading', { name: 'To Do list' }).click();
 });
 
+
 test('add and delete task', async ({ page, browser }) => {
-    await page.goto('http://localhost:3000/');
+    await page.goto('https://is373-t3-stack.vercel.app/');
 
-    // Detect the browser
-    const browserType = browser.browserType();
-
+    //add
     await page.getByRole('link', { name: 'add to do' }).click();
     await page.getByRole('textbox').click();
     await page.getByRole('textbox').fill('create12345');
     await page.getByRole('button', { name: 'add Todo' }).click();
     await expect(page.getByRole('cell', { name: 'create12345' })).toBeVisible();
+
+    //delete
     await page.getByRole('link', { name: 'delete' }).click();
-    if (browserType.name() == 'webkit'){
-      await page.locator(`xpath=//div[contains(@aria-label, 'create12345 Delete') or contains(text(), 'create12345 Delete')]`).locator('role=button').click();
-
-    }
-    else{
-      await page.locator(`role=row[name=/.*create12345 Delete$/]`).locator('role=button').click();
-    }
+    await page.locator(`role=row[name=/.*create12345 Delete$/]`).locator('role=button').click();
     await expect(page.getByRole('cell', { name: 'create12345' })).not.toBeVisible();
+  });
 
+
+test('todo list', async({page}) => {
+  await page.goto('https://is373-t3-stack.vercel.app/');
+
+  //add
+  await page.getByRole('link', { name: 'add to do' }).click();
+  await page.getByRole('textbox').click();
+  await page.getByRole('textbox').fill('create12347');
+  await page.getByRole('button', { name: 'add Todo' }).click();
+
+  //check new task appears on todolist
+  await page.getByRole('link', { name: 'toDo list' }).click();
+  await expect(page.getByRole('cell', { name: 'create12347' })).toBeVisible();
+
+  });
+
+
+test('add edit and delete task', async ({ page }) => {
+    try{
+    
+      await page.goto('https://is373-t3-stack.vercel.app/');
+      //add
+      await page.getByRole('link', { name: 'add to do' }).click();
+      await page.getByRole('textbox').click();
+      await page.getByRole('textbox').fill('create12346');
+      await page.getByRole('button', { name: 'add Todo' }).click();
+      await expect(page.getByRole('cell', { name: 'create12346' })).toBeVisible();
+
+      //edit
+      await page.getByRole('link', { name: 'edit' }).click();
+      await page.locator(`role=row[name=/.*create12346 Edit Todo$/]`).locator('role=textbox').click();
+      await page.locator(`role=row[name=/.*create12346 Edit Todo$/]`).locator('role=textbox').fill('create54321');
+      await page.locator(`role=row[name=/.*create54321 Edit Todo$/]`).locator('role=button').click();
+      await expect(page.getByRole('cell', { name: 'create54321' })).toBeVisible();
+
+      
+      //delete
+      await page.getByRole('link', { name: 'delete' }).click();
+      await page.locator(`role=row[name=/.*create54321 Delete$/]`).locator('role=button').click();
+      await expect(page.getByRole('cell', { name: 'create54321' })).not.toBeVisible();
+    }
+    catch(error){
+      //extra cleanup
+      if (await page.getByRole('cell', { name: 'create12346' }).isVisible()){
+        await page.getByRole('link', { name: 'delete' }).click();
+        await page.locator(`role=row[name=/.*create12346 Delete$/]`).locator('role=button').click();
+      }
+      throw error;
+    }
   });
